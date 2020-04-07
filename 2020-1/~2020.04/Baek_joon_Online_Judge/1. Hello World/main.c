e23171736fa0191a0bc33fe31bb109f4a211a684
Somethingfromn <somethingfromn@gmail.com>
2020-04-07 18:20:45 +0900

    test
    -cheking

---
+++ 2020-1/~2020.04/Baek_joon_Online_Judge/1. Hello World/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main(){
+
+  int column;
+  int row;
+  int star_counter;
+
+  scanf("%d", &star_counter);
+
+  for(row=star_counter; row >0; row--){
+
+
+    for(column = row; column < star_counter; column++){
+
+      printf(" ");
+
+    }
+
+    for(column = (row*2)-1; column>0; column--){
+
+      printf("*");
+
+    }
+    if(row != 1){
+      printf("\n");
+    }
+  }
+
+  for(row=0; row <= star_counter; row++){
+
+    if(row !=1){
+      for(column = row; column < star_counter; column++){
+
+        printf(" ");
+
+      }
+    
+      for(column = (row*2)-1; column>0; column--){
+      
+        printf("*");
+    
+
+      }
+    
+      if(row != star_counter){
+       printf("\n");
+      }
+    }
+  }
+
+
+
+  return 0;
+
+
+}

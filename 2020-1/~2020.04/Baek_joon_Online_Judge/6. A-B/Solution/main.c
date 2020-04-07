538cd0a1996ca21161f2b0c9493e20238ed5a360
Somethingfromn <58179041+somethingfromn@users.noreply.github.com>
2020-04-07 19:26:00 +0900

    Update main.c

--- 2020-1/~2020.04/Baek_joon_Online_Judge/6. A-B/Solution/main.c
+++ 2020-1/~2020.04/Baek_joon_Online_Judge/6. A-B/Solution/main.c
@@ -1,8 +1,21 @@
+/*문제
+두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+첫째 줄에 A-B를 출력한다.*/
 #include <stdio.h>
 
 int main(){
-    printf("강한친구 대한육군\n");
-    printf("강한친구 대한육군\n");
+    int A;
+    int B;
     
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    if(A>0 && B <10){
+        printf("%d", A-B);
+    }else{
+        printf("Error");
+        return 0;
+    }
     return 0;
 }

caea619756105c73dc95b8210844a2d127f6134b
Somethingfromn <58179041+somethingfromn@users.noreply.github.com>
2020-04-07 19:23:35 +0900

    Update main.c

--- 2020-1/~2020.04/Baek_joon_Online_Judge/5. A+B/Solution/main.c
+++ 2020-1/~2020.04/Baek_joon_Online_Judge/5. A+B/Solution/main.c
@@ -1,8 +1,20 @@
+//첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
 #include <stdio.h>
 
 int main(){
-    printf("강한친구 대한육군\n");
-    printf("강한친구 대한육군\n");
+    int A=0;
+    int B=0;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    if(A>0 && B<10){
+
+    printf("%d", A+B);
+    
+    }else{
+      printf("error");
+    }
     
     return 0;
 }

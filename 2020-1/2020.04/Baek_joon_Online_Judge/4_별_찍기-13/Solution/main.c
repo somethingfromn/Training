372a90186e48c9e8ad558ddabc569a8cbedda5ea
Somethingfromn <58179041+somethingfromn@users.noreply.github.com>
2020-04-13 00:16:08 +0900

    Update main.c

--- 2020-1/2020.04/Baek_joon_Online_Judge/4_별_찍기-13/Solution/main.c
+++ 2020-1/2020.04/Baek_joon_Online_Judge/4_별_찍기-13/Solution/main.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
 
 int main(){
-    printf("강한친구 대한육군\n");
-    printf("강한친구 대한육군\n");
+
+  int column;
+  int row;
+  int star_counter;
+
+
+  scanf("%d", &star_counter);
+
+  for(column =1; column <= star_counter; column++){
+
+   
+
+    for(row=0; row < column; row++){
+      
+      printf("*");
+
+      
+    }
+
     
-    return 0;
+
+    printf("\n");
+
+  }
+
+ 
+
+  for(column =star_counter-1; column > 0; column--){
+
+   
+
+    for(row=column; row > 0; row--){
+     
+      printf("*");
+
+      
+    }
+
+    
+
+    printf("\n");
+
+  }
+
+  return 0;
+
+
 }

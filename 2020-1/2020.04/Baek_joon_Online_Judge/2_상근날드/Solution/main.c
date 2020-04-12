428ded6fd8aa91f5870305170c36d25d000b483d
Somethingfromn <58179041+somethingfromn@users.noreply.github.com>
2020-04-13 00:10:34 +0900

    Update main.c

--- 2020-1/2020.04/Baek_joon_Online_Judge/2_상근날드/Solution/main.c
+++ 2020-1/2020.04/Baek_joon_Online_Judge/2_상근날드/Solution/main.c
@@ -1,8 +1,60 @@
 #include <stdio.h>
 
 int main(){
-    printf("강한친구 대한육군\n");
-    printf("강한친구 대한육군\n");
+
+  int price;
+  int drink_price=0;
+  int burger_price=0;
+  int set_price;
+  int count;
+
+
+  for(count=0; count<3; count++){
+
+
+    scanf("%d", &price);
     
-    return 0;
+    if(count == 0){
+      burger_price = price;
+    }
+
+    if(burger_price > price){
+
+      burger_price = price;
+
+    }
+  }
+
+  for(count =0; count<2; count++){
+    
+    scanf("%d", &price);
+
+    if(count == 0){
+      drink_price = price;
+    }
+
+    if(drink_price > price){
+      
+      drink_price = price;
+
+    }
+
+  }
+
+  set_price = burger_price + drink_price -50;
+
+  printf("%d", set_price);
+
+
+
+  return 0;
+
+
+
+
+
+
+  
+
+
 }

addeedd1aaf5429bf45374f48eca240d0b7cbd24
Somethingfromn <58179041+somethingfromn@users.noreply.github.com>
2020-04-16 10:36:29 +0900

    Update main.c

--- 2020-1/2020.04/Baek_joon_Online_Judge/7_최소_최대/Solution/main.c
+++ 2020-1/2020.04/Baek_joon_Online_Judge/7_최소_최대/Solution/main.c
@@ -1,23 +1,41 @@
-/*문제
-두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
-첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
-
-첫째 줄에 A×B를 출력한다.*/
-
 #include <stdio.h>
 
-int main(){
-    int A;
-    int B;
-    
-    scanf ("%d", &A);
-    scanf ("%d", &B);
-    
-    if (A > 0 && B <10){
-        printf("%d", A*B);
-    }else{
-        printf("error");
+
+int main(void) {
+
+  int N;
+  int a;
+  int i;
+
+  scanf("%d", &N);
+
+  int Array[N];
+  int Store_Number=0;
+  int Max=0;
+  int Min=0;
+
+  for (i=0; i<N; i++){
+    scanf("%d", &a);
+
+    Array [i] = a;
+
+  }
+
+  Max = Array[0];
+  Min = Array[0];
+
+  for (i=0; i<N; i++){
+
+    if (Array[i]>Max){
+      Max = Array[i];
     }
-    
-    return 0;
+    if (Array[i]<Min){
+      Min = Array[i];
+    }
+  }
+
+  printf("%d %d", Min, Max);
+
+
+  return 0;
 }

5725cd7e981a0096f092e3eb2125831681878fe7
Somethingfromn <58179041+somethingfromn@users.noreply.github.com>
2020-04-22 15:12:37 +0900

    Update main.c

--- 2020-1/2020.04/Baek_joon_Online_Judge/13_평균은_넘겠지/Solution/main.c
+++ 2020-1/2020.04/Baek_joon_Online_Judge/13_평균은_넘겠지/Solution/main.c
@@ -1,32 +1,58 @@
-/*문제
-시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
-
-입력
-첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
-
-출력
-시험 성적을 출력한다.*/
-
 #include <stdio.h>
 
-int main(){
-    
-    int Score;
-    
-    scanf("%d", &Score);
-    
-    if(90<= Score && 100 >= Score){
-        printf("A");
-    }else if(80 <= Score && 89 >= Score){
-        printf("B");
-    }else if(70<= Score && 79 >= Score){
-        printf("C");
-    }else if(60<= Score && 69 >= Score){
-        printf("D");
-    }else{
-        printf("F");
+int SizeOfArray = 1000;
+
+int main(void){
+
+  int C;
+  int a;
+  int b;
+  int c;
+  float count;
+  float HowMany;
+  float scores[SizeOfArray];
+  float average[SizeOfArray];
+  float PercentOfOverAverage[SizeOfArray];
+
+  scanf("%d", &C);
+
+  for(a=0; a<SizeOfArray; a++){
+    scores[a]= 'X';
+    average[a]= 'X';
+  }
+  
+  for(a=0; a<C; a++){
+    //쓰레기 값이 들어가있는 average의 초기화와 매번 초기화가 필요한 count
+    average[a]=0;
+    count=0;
+    scanf("%f", &HowMany);
+    if(HowMany < 1 || HowMany > 1000){
+      printf("\nError on inputting the number of cases");
     }
-    
-    return 0;
-    
+    for(b=0; b<HowMany; b++){
+      scanf("%f", &scores[b]);
+      if(scores[b] < 0 || scores[b] > 100){
+        printf("\nError: Scores should be bigger than or equal to 0 and smaller than or euqal to 100 at the same time");
+      }
+      //printf("\nscore: %f\n", ArrayOfScores[b]);
+      //printf("\nAverage: %f\n", ArrayOfAverage[0]);
+      average[a] += scores[b]; 
+    }
+    //printf("\nAverage: %f\n", ArrayOfAverage[0]);
+    average[a] = average[a]/HowMany;
+    for(c=0; c<HowMany; c++){
+      if(scores[c] > average[a]){
+        count ++;
+      }
+    }
+    //printf("\naverage: %f\n", average[a]);
+    //printf("\ncount: %f\n", count);
+    PercentOfOverAverage[a] = (count/HowMany)*100; 
+  }
+
+  for(a=0; a<C; a++){
+    printf("%.3f%%\n",PercentOfOverAverage[a]);  
+  }
+
+
 }

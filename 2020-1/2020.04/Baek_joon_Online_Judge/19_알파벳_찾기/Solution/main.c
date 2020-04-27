98bc4518f1610cd15c0df1a28e82dfa13ca1a0f8
Somethingfromn <58179041+somethingfromn@users.noreply.github.com>
2020-04-27 16:30:54 +0900

    Update main.c

--- 2020-1/2020.04/Baek_joon_Online_Judge/19_알파벳_찾기/Solution/main.c
+++ 2020-1/2020.04/Baek_joon_Online_Judge/19_알파벳_찾기/Solution/main.c
@@ -1,26 +1,49 @@
-/*문제
-n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
-
-입력
-첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
-
-출력
-1부터 n까지 합을 출력한다.*/
-
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-    int n;
-    int count;
-    int sum=0;
+#define NumOfAlpha 26
+
+int main(void){
+
+  int a;
+  int count=0;
+  int AsciiFirstNumOfAlpha=97;
+  char S;
+  char Alpha[NumOfAlpha];
+  int Alpha2[NumOfAlpha];
+  char Word;
+
+  // 각 배열에 알파벳 넣음
+  for(a=0; a<NumOfAlpha; a++){
     
-    scanf("%d", &n);
-    
-    for(count=1; count <= n; count++){
-        sum = sum + count;
+    Alpha[a]= AsciiFirstNumOfAlpha;
+    Alpha2[a] = -1;
+    AsciiFirstNumOfAlpha++;
+
+  }
+  //printf("%c", Alpha[0]);
+  while(1){
+    scanf("%c", &Word);
+    //printf("%c", Word);
+    if(Word == '\n'){
+      break;
     }
-    
-    printf("%d", sum);
-    
-    return 0;
+
+    for(a=0; a<26; a++){
+      if(Alpha[a] == Word){
+        //printf("%c", Alpha[a]);
+        //printf("%c", Word); 
+        //printf("%d", count);
+        //o가 연속해서 나오는 경우에는 가장 늦게 나온 count를 저장하기 때문, 이미 -1이 아닌 것은 따로 또 구할필요 없다.
+        if(Alpha2[a] == -1){
+          Alpha2[a] = count;
+        }
+      }
+    }
+    count++;
+  }
+  for(a=0; a<NumOfAlpha; a++){
+    printf("%d ", Alpha2[a]);
+  }
+  return 0;
 }
